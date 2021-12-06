c63c8039da32d12f20cfb7ef1fd7223face183e4
Shegun Babs <shegun.babs@gmail.com>
2021-12-06 10:24:15 +0100

    started writing main tests

--- tests/Feature/HelpersTest.php
+++ tests/Feature/HelpersTest.php
@@ -1,2 +1,51 @@
 <?php
 
+use Illuminate\Support\Carbon;
+use Shegunbabs\LaravelHelpers\LaravelHelpersServiceProvider;
+
+beforeEach(function (){
+    (new LaravelHelpersServiceProvider(null))->boot();
+});
+
+
+test('carbon', function ()
+{
+    $this->assertInstanceOf(Carbon::class, carbon());
+    $this->assertEquals(Carbon::parse('today'), carbon('today'));
+});
+
+
+test('chain', function()
+{
+
+});
+
+
+test('connection', function()
+{
+
+});
+
+
+test('dump_sql', function ()
+{
+
+});
+
+
+test('emoji', function ()
+{
+
+});
+
+
+test('object', function()
+{
+
+});
+
+
+test('ordinal', function()
+{
+
+});
