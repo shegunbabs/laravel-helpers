2ccfbf97772642ca4ef1822e579d6dd75d47a0b8
Shegun Babs <shegun.babs@gmail.com>
2021-12-06 12:50:47 +0100

    add object test

--- tests/Feature/HelpersTest.php
+++ tests/Feature/HelpersTest.php
@@ -18,18 +18,27 @@ test('carbon', function ()
 test('chain', function()
 {
 
+    $mock = \Mockery::mock();
+    $mock->shouldReceive('first')->once()->andReturn('first thing');
+    $mock->shouldReceive('second')->once()->with('first thing');
+    $mock->shouldReceive('third')->once()->andReturn('third thing');
+
+    $result = chain($mock)->first()->second(carry)->third()();
+
+    $this->assertEquals('third thing', $result);
+
 });
 
 
 test('connection', function()
 {
-
+    $this->assertTrue(function_exists('connection'));
 });
 
 
 test('dump_sql', function ()
 {
-
+    $this->assertTrue(true);
 });
 
 
@@ -41,7 +50,13 @@ test('emoji', function ()
 
 test('object', function()
 {
+    $object = new StdClass();
+    $object->firstname = 'john';
+    $object->lastname = 'doe';
 
+    $array = ['firstname' => 'john', 'lastname' => 'doe'];
+
+    $this->assertEquals($object, object($array));
 });
 
 
